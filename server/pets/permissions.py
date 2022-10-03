748d22a0b56121fb79ee15b5b48d52182ecc48b8
natthaphongkongkaew <kongkaen@oregonstate.edu>
2022-10-02 20:42:13 -0700

    Import all files

---
+++ server/pets/permissions.py
@@ -0,0 +1,31 @@
+from rest_framework import permissions
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the pet.
+        return obj.owner == request.user
+
+class IsShelterCanCreate(permissions.BasePermission):
+    """
+    Custom permission to only allow shelters to create a Pet object.
+    """
+    def has_permission(self, request, view):
+        # Create permissions are only allowed to shelters
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        is_shelter = request.user.profile.is_shelter
+        if is_shelter:
+            return True
+        else:
+            return False
---
+++ server/pets/views.py
@@ -0,0 +1,82 @@
+from pets.models import Pet, Profile
+from pets.serializers import PetSerializer, UserSerializer, ProfileSerializer, RegisterSerializer, RegisterShelterSerializer
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
+from pets.permissions import IsOwnerOrReadOnly, IsShelterOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
+from .serializers import MyTokenObtainPairSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class Login(TokenObtainPairView):
+    permission_classes = [AllowAny]
+    serializer_class = MyTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
+
+class RegisterShelterSerializerView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permissions_classes = [AllowAny]
+    serializer_class = RegisterShelterSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class PetList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = PetSerializer
+
+    def get_queryset(self):
+        """
+        Sets queryset to all Pet Objects. Then checks for query params of 'breed', 'type' , 'availability',
+        'disposition', etc. If one of those is present, it alters the query according to that param and returns a new
+        queryset with the Pet objects filtered by that. Can only accept one query param to filter by.
+        """
+        queryset = Pet.objects.all()
+
+        # Tries to get data from the query params
+        breed = self.request.query_params.get('breed')
+        type = self.request.query_params.get('type')
+        availability = self.request.query_params.get('availability')
+        disposition = self.request.query_params.get('disposition')
+
+        # Alters queryset according to the presence of a query param
+        if breed is not None:
+            queryset = queryset.filter(breed=breed)
+        elif type is not None:
+            queryset = queryset.filter(type=type)
+        elif availability is not None:
+            queryset = queryset.filter(availability=availability)
+        elif disposition is not None:
+            queryset = queryset.filter(disposition=disposition)
+
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PetDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+    queryset = Pet.objects.all()
+    serializer_class = PetSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'pets': reverse('pet-list', request=request, format=format),
+    })
